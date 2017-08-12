840ad71159fc9f04b47311866c6680a91dade095
R.J Lélis <renatojlelis@gmail.com>
2017-08-12 10:45:25 -0300

    Update exercicio 4.cpp

--- exercicio 4.cpp
+++ exercicio 4.cpp
@@ -1,14 +1,13 @@
+// Implemente um programa em C que receba como entrada os catetos de um triângulo retângulo e calcule a sua hipotenusa
 #include<iostream>
 #include<math.h>
 using namespace std;
 main(){
-	float ca,co,h;
-	cout<<"digite o cateto adjacente: "<<"\n";
-	cin>>ca;
-	cout<<"digite o cateto oposto: "<<"\n";
-	cin>>co;
-	h=sqrt(ca*ca+co*co);
-	cout<<"resultado: "<<h;
-	
-	
+	float ca, co, hipo;
+	cout << "Digite o cateto adjacente: ";
+	cin >> ca;
+	cout << "Digite o cateto oposto: ";;
+	cin >> co;
+	hipo = sqrt(ca * ca + co * co);
+	cout << "Hipotenusa do triangulo retangulo: " << hipo;
 }
