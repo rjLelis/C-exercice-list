0497f08162a9c60f61dc8eea18ca580cdb1015fe
R.J Lélis <renatojlelis@gmail.com>
2017-08-12 10:58:05 -0300

    Update exercicio 6.cpp

--- exercicio 6.cpp
+++ exercicio 6.cpp
@@ -1,11 +1,17 @@
+/*Programa em C que receba dois valores e calcule a expressão abaixo:
+
+   sen(x²) + cos(x)²
+   ----------------
+       3√2 + √7
+*/
 #include<iostream>
 #include<math.h>
 using namespace std;
 main(){
-	float x,y,r;
-	cout<<"digite dois numeros: "<<"\n";
-	cin>>x>>y;
-	r=(sin(x*x)+cos(x)*cos(x)/3*sqrt(2)+sqrt(7));
-	cout<<"resultado: "<<r<<"\n";
+	float x, y ,res;
+	cout<<"Digite dois numeros: ";;
+	cin >> x >> y;
+	res = ((sin(x * x) + cos(x) * cos(x)) / (3 * sqrt(2)+sqrt(7)));
+	cout << "Resultado: " << res <<"\n";
 	
 }
