d87721460ebd16e2a064cacfc3d3ea6df6a5e577
R.J Lélis <renatojlelis@gmail.com>
2017-08-12 10:55:46 -0300

    Update exercicio 5.cpp

--- exercicio 5.cpp
+++ exercicio 5.cpp
@@ -1,10 +1,16 @@
+/*Programa em C que receba dois valores e calcule a expressão abaixo:
+
+   sen(x)√2
+   --------  - cos(y)
+     3√3
+*/
 #include<iostream>
 #include<math.h>
 using namespace std;
 main(){
-	float x,y,r;
-	cout<<"digite dois numeros: "<<"\n";
-	cin>>x>>y;
-	r=(sin(x)+sqrt(2))/(3*sqrt(3))-cos(y);
-	cout<<"resultado: "<<r<<"\n";
+	float x, y ,res;
+	cout << "Digite dois numeros: ";
+	cin >> x >> y;
+	res = (sin(x) + sqrt(2)) / (3 * sqrt(3)) - cos(y);
+	cout << "Resultado: " <<res << "\n";
 }
