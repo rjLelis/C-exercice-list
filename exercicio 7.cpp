f5c314452cb89d6795a6c769b22dd34cfd0bb1d4
R.J Lélis <renatojlelis@gmail.com>
2017-08-12 11:00:31 -0300

    Update exercicio 7.cpp

--- exercicio 7.cpp
+++ exercicio 7.cpp
@@ -1,10 +1,17 @@
+/*Programa em C que receba três valores e calcule a expressão abaixo:
+
+ √(cos(x)² + sen(y)² + sen(z²)²)
+ -------------------------------
+     2 + cos(3) + cos(4)
+*/
+
 #include<iostream>
 #include<math.h>
 using namespace std;
 main(){
-	float x,y,z,r;
-	cout<<"digite tres numeros: "<<"\n";
-	cin>>x>>y>>z;
-	r=sqrt(cos(x)*cos(y)+sin(y)*sin(y)+sin(z*z)*sin(z*z))/(2+cos(3)+cos(4));
-	cout<<"resultado: "<<r<<"\n";
+	float x, y, z, res;
+	cout<<"Digite tres numeros: ";
+	cin >> x >> y >> z;
+	res = sqrt(cos(x) * cos(y) + sin(y) * sin(y) + sin (z * z) * sin(z * z)) / (2 + cos(3) + cos(4));
+	cout << "Resultado: " << res << "\n";
 }
