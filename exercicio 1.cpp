52cc029abdcf07c8b693830912029162c90322d0
R.J Lélis <renatojlelis@gmail.com>
2017-08-12 10:38:28 -0300

    Update exercicio 1.cpp

--- exercicio 1.cpp
+++ exercicio 1.cpp
@@ -1,9 +1,10 @@
+// Implemente um programa em C que receba como entrada o lado de um quadrado e calcule e escreva a sua área
 #include<iostream>
 using namespace std;
 main(){
-	float lado,area;
-	cout<<"digite o valor do lado: "<<"\n";
-	cin>>lado;
-	area=lado*lado;
-	cout<<"resultado: "<<area<<"\n";
+	float lado, area;
+	cout << "digite o lado do quadrado: ;
+	cin >> lado;
+	area = lado * lado;
+	cout<< "Area do quadrado: " << area << "\n";
 }
