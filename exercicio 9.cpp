1d13a211b50f6f7789777a9bc0dd73e5fd02a950
R.J Lélis <renatojlelis@gmail.com>
2017-08-12 21:14:08 -0300

    Update exercicio 9.cpp

--- exercicio 9.cpp
+++ exercicio 9.cpp
@@ -1,20 +1,24 @@
+/*Em uma determinada cidade existem 4 canais de TV. 
+Implemente um programa em C que receba como entrada o número de casas com a TV ligada em cada canal às 20:00hs de um domingo,
+e calcule e escreva os percentuais de audiência de cada um dos canais
+*/
 #include<iostream>
 #include<math.h>
 using namespace std;
 main(){
-	int q1,q2,q3,q4,t;
-	cout<<"digite a quantidade do canal1: "<<"\n";
-	cin>>q1;
-	cout<<"digite a quantidade do canal2: "<<"\n";
-	cin>>q2;
-	cout<<"digite a quantidade do canal3: "<<"\n";
-	cin>>q3;
-	cout<<"digite a quantidade do canal4: "<<"\n";
-	cin>>q4;
-	t=q1+q2+q3+q4;
-	cout<<"audiencia do canal1: "<<q1*100/t<<"\n";
-	cout<<"audiencia do canal2: "<<q2*100/t<<"\n";
-	cout<<"audiencia do canal3: "<<q3*100/t<<"\n";
-	cout<<"audiencia do canal4: "<<q4*100/t<<"\n";
+	int canal1,canal2,canal3,canal4,t;
+	cout << "Digite a quantidade do canal1: ";
+	cin >> canal1;
+	cout << "Digite a quantidade do canal2: ";
+	cin >> canal2;
+	cout << "Digite a quantidade do canal3: ";
+	cin >> canal3;
+	cout << "Digite a quantidade do canal4: ";
+	cin >> canal4;
+	t = canal1 + canal2 + canal3 + canal4;
+	cout << "Audiencia do canal 1: " << canal1 * 100 / t <<"\n";
+	cout << "Audiencia do canal 2: " << canal2 * 100 / t <<"\n";
+	cout << "Audiencia do canal 3: " << canal3 * 100 / t << "\n";
+	cout << "Audiencia do canal 4: " << canal4 * 100 / t << "\n";
 	
 }
