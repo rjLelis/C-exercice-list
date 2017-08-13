6688052db054f7d6710d0ae78d1d3d8de4263c96
R.J Lélis <renatojlelis@gmail.com>
2017-08-12 21:18:30 -0300

    Update exercicio 10.cpp

--- exercicio 10.cpp
+++ exercicio 10.cpp
@@ -1,14 +1,23 @@
+/*Implemente um programa em C que receba como entrada a quantidade total de produtos de um determinado tipo vendidos por um vendedor.
+Sabendo que o valor unitário de cada produto é de R$ 10,00, calcule e escreva:
+
+a. O valor total da venda
+
+b. Sabendo que a comissão do vendedor é de 10%, calcule quanto ele irá ganhar
+
+c. Sabendo que o preço de venda do produto é R$ 2,50 mais caro que o de compra, determine o lucro que o vendedor deu para a empresa*/
 #include<iostream>
 #include<math.h>
 using namespace std;
 main(){
-	float quttotal,v,c,l;
-	cout<<"digite a quantidade total de produtos: "<<"\n";
-	cin>>quttotal;
-	v=quttotal*10;
-	c=v*0.1;
-	l=v*2.50;
-	cout<<"valor total da compra: "<<v<<"\n";
-	cout<<"comissao do vendedor: "<<c<<"\n";
-	cout<<"lucro: "<<l<<"\n";
+	int quantidade_total;
+	float valor,comissao,lucro;
+	cout << "Digite a quantidade total de produtos: "<<"\n";
+	cin >> quantidade_total;
+	valor = quantidade_total * 10;
+	c = valor * 0.1;
+	lucro = v * 2.50;
+	cout << "Valor total da compra: "<<valor << "\n";
+	cout << "Comissao do vendedor: " << comissao << "\n";
+	cout << "Lucro: " << lucro << "\n";
 }
